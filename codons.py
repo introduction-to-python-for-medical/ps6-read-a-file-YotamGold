650c80a485b082cb97207530f511be122a28562d
YotamGold <yotam.goldberg@gmail.com>
2024-12-09 17:40:05 +0200

    Update codons.py1

--- codons.py
+++ codons.py
@@ -1,4 +1,9 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
-
+    file = open(file_path, "r")
+    rows = file.readlines()
+    file.close()
+    myD = {}
+    for row in rows:
+      cell = row.strip().split('\t')
+      myD[cell[0]] = cell [2] 
+    return myD
